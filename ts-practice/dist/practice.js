a31f869a1efc4b3399becb1d1fff44ca96e95f2f
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-05-27 22:28:26 +0900

    [ADD] interface 예제

--- ts-practice/dist/practice.js
+++ ts-practice/dist/practice.js
@@ -1,3 +1,26 @@
 "use strict";
-const message = "hello worlds";
-console.log(message);
+//숫자 합 구하기
+// function sumArr(number:number[]):number{
+//     return number.reduce((acc,current) => acc+current,0)
+// }
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.height * this.width;
+    }
+}
+const circle = new Circle(5);
+const rectangle = new Rectangle(5, 10);
+const shapes = [circle, rectangle];
+shapes.forEach(shape => console.log(shape.getArea()));
--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,8 +1,44 @@
 //숫자 합 구하기
-function sumArr(number:number[]):number{
-    return number.reduce((acc,current) => acc+current,0)
+// function sumArr(number:number[]):number{
+//     return number.reduce((acc,current) => acc+current,0)
+// }
+
+// const sum =  sumArr([1,2,3,4,5])
+
+// console.log(sum)
+
+//interface 사용
+interface Shape{
+    getArea():number
 }
 
-const sum =  sumArr([1,2,3,4,5])
+class Circle implements Shape {
+    radius:number
 
-console.log(sum)
+    constructor(radius:number){
+     this.radius = radius   
+    }
+    getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+}
+
+class Rectangle implements Shape {
+    width:number
+    height:number
+
+    constructor(width:number, height:number){
+        this.width = width
+        this.height = height
+    }
+    
+    getArea(){
+        return this.height * this.width
+    }
+}
+
+const circle = new Circle(5)
+const rectangle = new Rectangle(5,10)
+const shapes = [circle,rectangle]
+
+shapes.forEach(shape => console.log(shape.getArea()))
